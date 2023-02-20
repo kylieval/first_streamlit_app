134218046ad340a9335ab443dc4ba54e261965f3
kylieval <125923153+kylieval@users.noreply.github.com>
2023-02-20 15:29:04 -0600

    Update streamlit_app.py
    forgot to add a ) at the end of the smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🍓Build Your Own Fruit Smoothie🥝🍇'
+streamlit.header('🍌🍓Build Your Own Fruit Smoothie🥝🍇')
